7cf3ee0283cfe8e998f2a5a424bab3e8dc2c268d
Mattias Bergström <bergstroem1@gmail.com>
2014-12-06 19:38:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# flow-sdk
+
+JavaScript SDK for Flow
+
+## About
+
+A JavaScript library by Mattias Bergström.
+
+See the [project homepage](http://bergstroem1.github.io/flow-sdk).
+
+## Installation
+
+Using Bower:
+
+    bower install flow-sdk
+
+Or grab the [source](https://github.com/bergstroem1/flow-sdk/dist/flow-sdk.js) ([minified](https://github.com/bergstroem1/flow-sdk/dist/flow-sdk.min.js)).
+
+## Usage
+
+Basic usage is as follows:
+
+    flow-sdk();
+
+For advanced usage, see the documentation.
+
+## Documentation
+
+Start with `docs/MAIN.md`.
+
+## Contributing
+
+We'll check out your contribution if you:
+
+* Provide a comprehensive suite of tests for your fork.
+* Have a clear and documented rationale for your changes.
+* Package these up in a pull request.
+
+We'll do our best to help you out with any contribution issues you may have.
+
+## License
+
+MIT. See `LICENSE.txt` in this directory.
---
+++ src/main.js
@@ -0,0 +1,75 @@
+/* flow-sdk main */
+
+// Base function.
+var flowSdk = function() {
+  var httpRequest;
+  var baseUrl = 'http://today.emilsjolander.me';
+
+  // Add functionality here.
+  var performRequest = function(method, url, data, success, error) {
+    success = success || function() {};
+    error = error || function() {};
+
+
+    if (window.XMLHttpRequest) {
+      httpRequest = new window.XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+      try {
+        httpRequest = new window.ActiveXObject("Msxml2.XMLHTTP");
+      }
+      catch (e) {
+        try {
+          httpRequest = new window.ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch (e) {}
+      }
+    }
+
+    if (!httpRequest) {
+      error('Giving up :( Cannot create an XMLHTTP instance');
+    }
+    httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          success(httpRequest.responseText);
+        } else {
+          error('There was a problem with the request.');
+        }
+      }
+    };
+    httpRequest.open(method, url);
+    httpRequest.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    httpRequest.send(JSON.stringify(data));
+  };
+
+  var log = function(endpoint, data, success, error) {
+    var url = baseUrl + endpoint;
+    performRequest('POST', url, data, success, error);
+  };
+
+  var enter = function(view) {
+    log('/enter', {
+      view: view
+    });
+  };
+
+  var navigate = function(action, view) {
+    log('/navigate', {
+      action: action,
+      view: view
+    });
+  };
+
+  return {
+    navigate: navigate,
+    enter: enter
+  };
+};
+
+
+// Version.
+flowSdk.VERSION = '0.0.1';
+
+
+// Export to the root, which is probably `window`.
+root.flowSdk = flowSdk;
